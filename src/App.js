15ed9a46b3b55b16800ef593fe6cd986e7dca676
Lina Djihane <lina.djihane@gmail.com>
2021-02-19 19:59:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Profile from './profile/Profile';
 
 function App() {
+  const handleName = name => alert(name);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile 
+        fullName="John Doe" 
+        profession="Web Developer"
+        bio="Cat lover, bookworm, apple pie fanatic and nature enthousiast :)"
+        handleName={handleName}>
+          https://randomuser.me/api/portraits/men/32.jpg 
+      </Profile>
     </div>
   );
 }
 
 export default App;
+
+/*
+-Create a project using create-react-app.
+-Create profile folder under src.
+-Create Profile component under /profile.
+
+-Profile component get data (fullName, bio, profession) as props, and get image as 
+children props, and function handleName function as props that alert name 
+of profile user.
+
+-Define default props for Profile component. 
+-Use PropsType to check props.  
+-Use inline style.
+*/
---
+++ src/profile/Profile.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const cardStyle = { 
+    border: '2px solid #17a589',
+    boxShadow: '5px 6px 0px #17a589',
+    borderRadius: '3px',
+    padding: '21px',
+    textAlign: 'center',
+    width: '340px' };
+const imgStyle = {borderRadius: '50%', border: '4px solid #17a589'};
+
+function Profile(props) {
+    props.handleName(props.fullName);
+    return (
+        <div style={cardStyle}>
+            <div>
+                <img 
+                    src={props.children}
+                    alt="avatar"
+                    style = {imgStyle} >
+                </img>
+            </div>
+            <br></br>
+            <div>
+                <h1 style={{color : '#17a589'}}>{props.fullName}</h1>
+            </div>
+            <div>
+                <p style={{fontSize: '.8rem'}}>{props.profession}</p>
+            </div>
+            <br></br>
+            <div>
+                <p>{props.bio}</p>
+            </div>
+        </div>
+    );
+}
+
+Profile.defaultProps = { children : "https://ih1.redbubble.net/image.1046392292.3346/flat,128x128,075,t.jpg"}
+
+export default Profile;
